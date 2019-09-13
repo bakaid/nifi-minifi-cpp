f6078a238184e6eb135f1c8f7be25307aec5bd17
Daniel Bakai <bakaidl@gmail.com>
2019-09-13 16:32:54 +0200

    civetweb

--- cmake/BundledCivetWeb.cmake
+++ cmake/BundledCivetWeb.cmake
@@ -18,29 +18,31 @@
 function(use_bundled_civetweb SOURCE_DIR BINARY_DIR)
     message("Using bundled civetweb")
 
+    # Patch source
     if (WIN32)
-        set(BYPRODUCTS
-                "lib/libcivetweb.lib"
-                "lib/libcivetweb-cpp.lib"
-                )
     else()
-        set(BYPRODUCTS
-                "lib/libcivetweb.a"
-                "lib/libcivetweb-cpp.a"
-                )
+        set(PC patch -p1 < ${SOURCE_DIR}/thirdparty/civetweb/civetweb.patch)
     endif()
+
+    # Define byproducts
+    if (WIN32)
+        set(SUFFIX "lib")
+    else()
+        set(SUFFIX "a")
+    endif()
+
+    set(BYPRODUCTS
+            "lib/libcivetweb.${SUFFIX}"
+            "lib/libcivetweb-cpp.${SUFFIX}"
+            )
+
+    set(CIVETWEB_BIN_DIR "${BINARY_DIR}/thirdparty/civetweb-install/" CACHE STRING "" FORCE)
 
     FOREACH(BYPRODUCT ${BYPRODUCTS})
-        LIST(APPEND CIVETWEB_LIBRARIES_LIST "${CMAKE_CURRENT_BINARY_DIR}/thirdparty/civetweb-install/${BYPRODUCT}")
+        LIST(APPEND CIVETWEB_LIBRARIES_LIST "${CIVETWEB_BIN_DIR}/${BYPRODUCT}")
     ENDFOREACH(BYPRODUCT)
 
-    if (WIN32)
-    else()
-        set(PC patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/civetweb/civetweb.patch)
-    endif()
-
-    set(CIVETWEB_BIN_DIR "${CMAKE_CURRENT_BINARY_DIR}/thirdparty/civetweb-install/" CACHE STRING "" FORCE)
-
+    # Set build options
     set(CIVETWEB_CMAKE_ARGS ${PASSTHROUGH_CMAKE_ARGS}
             "-DCMAKE_INSTALL_PREFIX=${CIVETWEB_BIN_DIR}"
             -DCMAKE_POSITION_INDEPENDENT_CODE=ON
@@ -49,8 +51,11 @@ function(use_bundled_civetweb SOURCE_DIR BINARY_DIR)
             -DBUILD_TESTING=OFF
             -DCIVETWEB_ENABLE_ASAN=OFF # TODO
             )
+    if (OPENSSL_OFF)
+        list(APPEND CIVETWEB_CMAKE_ARGS -DCIVETWEB_ENABLE_SSL=OFF)
+    endif()
 
-    list(APPEND CMAKE_MODULE_PATH_PASSTHROUGH_LIST ${CMAKE_CURRENT_SOURCE_DIR}/cmake/ssl)
+    list(APPEND CMAKE_MODULE_PATH_PASSTHROUGH_LIST ${SOURCE_DIR}/cmake/ssl)
     list(APPEND CIVETWEB_CMAKE_ARGS "-DLIBRESSL_BIN_DIR=${LIBRESSL_BIN_DIR}"
             "-DLIBRESSL_SRC_DIR=${LIBRESSL_SRC_DIR}"
             "-DBYPRODUCT_PREFIX=${BYPRODUCT_PREFIX}"
@@ -60,43 +65,40 @@ function(use_bundled_civetweb SOURCE_DIR BINARY_DIR)
         list(APPEND CIVETWEB_CMAKE_ARGS "-DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH_PASSTHROUGH}")
     endif()
 
+    # Build project
     ExternalProject_Add(
             civetweb-external
             URL "https://github.com/civetweb/civetweb/archive/v1.10.tar.gz"
-            SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/thirdparty/civetweb-src"
+            URL_HASH "SHA256=e6958f005aa01b02645bd3ff9760dd085e83d30530cdd97b584632419195bea5"
+            SOURCE_DIR "${BINARY_DIR}/thirdparty/civetweb-src"
             LIST_SEPARATOR % # This is needed for passing semicolon-separated lists
             CMAKE_ARGS ${CIVETWEB_CMAKE_ARGS}
             PATCH_COMMAND ${PC}
             BUILD_BYPRODUCTS "${CIVETWEB_LIBRARIES_LIST}"
     )
 
-    add_dependencies(civetweb-external libressl-portable)
-
-    add_library(c-library STATIC IMPORTED)
-    if (WIN32)
-        set_target_properties(c-library PROPERTIES IMPORTED_LOCATION "${CIVETWEB_BIN_DIR}/lib/libcivetweb.lib")
-    else()
-        set_target_properties(c-library PROPERTIES IMPORTED_LOCATION "${CIVETWEB_BIN_DIR}/lib/libcivetweb.a")
-    endif()
-
-    add_library(civetweb-cpp STATIC IMPORTED)
-    if (WIN32)
-        set_target_properties(civetweb-cpp PROPERTIES IMPORTED_LOCATION "${CIVETWEB_BIN_DIR}/lib/libcivetweb-cpp.lib")
-    else()
-        set_target_properties(civetweb-cpp PROPERTIES IMPORTED_LOCATION "${CIVETWEB_BIN_DIR}/lib/libcivetweb-cpp.a")
-    endif()
-
+    # Set variables
     set(CIVETWEB_FOUND "YES" CACHE STRING "" FORCE)
     set(CIVETWEB_INCLUDE_DIR "${CIVETWEB_BIN_DIR}/include" CACHE STRING "" FORCE)
-    set(CIVETWEB_LIBRARIES ${OPENWSMAN_LIBRARIES_LIST} CACHE STRING "" FORCE)
+    set(CIVETWEB_LIBRARIES "${CIVETWEB_BIN_DIR}/lib/libcivetweb.${SUFFIX}" "${CIVETWEB_BIN_DIR}/lib/libcivetweb-cpp.${SUFFIX}" CACHE STRING "" FORCE)
 
+    # Set exported variables for FindPackage.cmake
+    set(EXPORT_CIVETWEB_INCLUDE_DIR "${CIVETWEB_INCLUDE_DIR}" CACHE STRING "" FORCE)
+    set(EXPORT_CIVETWEB_LIBRARIES "${CIVETWEB_LIBRARIES}" CACHE STRING "" FORCE)
+
+    # Create imported targets
+    add_library(CIVETWEB::c-library STATIC IMPORTED)
+    set_target_properties(CIVETWEB::c-library PROPERTIES IMPORTED_LOCATION "${CIVETWEB_BIN_DIR}/lib/libcivetweb.${SUFFIX}")
     file(MAKE_DIRECTORY ${CIVETWEB_INCLUDE_DIR})
+    set_property(TARGET CIVETWEB::c-library APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CIVETWEB_INCLUDE_DIR})
+    add_dependencies(CIVETWEB::c-library civetweb-external)
+    if (NOT OPENSSL_OFF)
+        add_dependencies(civetweb-external OpenSSL::SSL OpenSSL::Crypto)
+        set_property(TARGET CIVETWEB::c-library APPEND PROPERTY INTERFACE_LINK_LIBRARIES OpenSSL::SSL OpenSSL::Crypto)
+    endif()
 
-    if (OPENSSL_FOUND)
-        set_property(TARGET c-library APPEND PROPERTY INTERFACE_LINK_LIBRARIES OpenSSL::SSL OpenSSL::Crypto)
-    endif(OPENSSL_FOUND)
-    set_property(TARGET c-library APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CIVETWEB_INCLUDE_DIR})
-    set_property(TARGET civetweb-cpp APPEND PROPERTY INTERFACE_LINK_LIBRARIES c-library)
-    add_dependencies(c-library civetweb-external)
-    add_dependencies(civetweb-cpp civetweb-external)
+    add_library(CIVETWEB::civetweb-cpp STATIC IMPORTED)
+    set_target_properties(CIVETWEB::civetweb-cpp PROPERTIES IMPORTED_LOCATION "${CIVETWEB_BIN_DIR}/lib/libcivetweb-cpp.${SUFFIX}")
+    set_property(TARGET CIVETWEB::civetweb-cpp APPEND PROPERTY INTERFACE_LINK_LIBRARIES CIVETWEB::c-library)
+    add_dependencies(CIVETWEB::civetweb-cpp civetweb-external)
 endfunction(use_bundled_civetweb)
