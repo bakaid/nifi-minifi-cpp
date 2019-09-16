b013439be9392dacfbf5c0291b749ff1efae6e76
Daniel Bakai <bakaidl@gmail.com>
2019-09-16 15:52:36 +0200

    fix

--- cmake/BundledCivetWeb.cmake
+++ cmake/BundledCivetWeb.cmake
@@ -46,6 +46,7 @@ function(use_bundled_civetweb SOURCE_DIR BINARY_DIR)
             -DCIVETWEB_ENABLE_SSL_DYNAMIC_LOADING=OFF
             -DCIVETWEB_ENABLE_CXX=ON
             -DBUILD_TESTING=OFF
+            -DCIVETWEB_ALLOW_WARNINGS=ON
             -DCIVETWEB_ENABLE_ASAN=OFF # TODO
             )
     if (OPENSSL_OFF)
